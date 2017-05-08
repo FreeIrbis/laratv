8f6ea6cbef3b14219d8fc6fb72471241c9fcb584
PleshakovV <pleshakov.valery@gmail.com>
2017-05-08 23:18:07 +0300

    Add change view and app files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravelsss
                 </div>
 
                 <div class="links">
