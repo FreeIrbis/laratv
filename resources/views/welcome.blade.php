c0ef0935a76f5a114536c8124316fc0b7dd1a5c8
FreeIrbis <mikhailovskiy.vladislav@gmail.com>
2017-05-08 23:22:06 +0300

    ITS MY CHANGEEEES

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravelsss
+                    Laravelss333
                 </div>
 
                 <div class="links">
