f429662534e49ea7c0892511b31b1ede9d45da58
FreeIrbis <mikhailovskiy.vladislav@gmail.com>
2017-05-10 00:01:05 +0300

    ZALUPA MODE ON

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    protected $redirectTo = '/home';
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getLogout()
+    {
+        $this->auth->logout();
+        Session::flush();
+        return redirect('/home');
+    }
+
+    public function index()
+    {
+        return view('admin.index');
+    }
+}
